89def5f831698010132b5bfcab3220e94809b59c
Daniel Cavazos <dcaazy@gmail.com>
2024-03-11 22:38:58 -0700

    Implement save test

---
+++ documents/notes.md
@@ -0,0 +1,6 @@
+# Scenes 
+Using scenes as a skeleton seems to make the most sense.
+
+Scripts attached to the top level node should initialize the data.
+
+# Scripts
--- scenes/debug_menu/debug_menu.gd
+++ scenes/debug_menu/debug_menu.gd
@@ -1,10 +1,16 @@
 extends Node2D
 var item_container:VBoxContainer
+var some_data:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	item_container = $CenterContainer/ItemContainer
 	add_menu_item("Test", test_function)
+	add_menu_item("Increment", func():
+		some_data += 1
+	)
+	add_menu_item("Save", save_data)
+	add_menu_item("Load", load_data)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +24,26 @@ func add_menu_item(text:String, callback:Callable):
 	item_container.add_child(menu_item)
 	
 func test_function():
-	print("Test function called.")
+	print("Test function called. ", some_data)
+
+func save_data():
+	var save:FileAccess = FileAccess.open("user://debug_save.json", FileAccess.WRITE)
+	var json:String = JSON.stringify(to_representation())
+	save.store_string(json)
+	save.close()
+	
+func load_data():
+	var save_string:String = FileAccess.get_file_as_string("user://debug_save.json")
+	var save_data:Dictionary = JSON.parse_string(save_string)
+	from_representation(save_data)
+	
+func to_representation() -> Dictionary:
+	# Serializes this node's saveable data into a dictionary.
+	return {
+		"some_data" : some_data
+	}
+
+func from_representation(data:Dictionary):
+	for key in data.keys():
+		print("Loading ",key,": ",data[key])
+		set(key, data[key])
